edc5b0ca4f1cd4e5f44086df87f68bee854399bc
jellyfishgh <1129088994@qq.com>
2016-11-09 17:29:45 +0800

    guide expression event

--- tool.js
+++ tool.js
@@ -1,9 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-fs.readDir(path.join(__dirname, 'public/html'), (err, files) => {
-    if(err) throw err;
+fs.readdir(path.join(__dirname, 'public/html'), (err, files) => {
+    if(err) throw err
+    const dirs = []
+    const purefiles = []
     files.map((file) => {
-        
-    });
-});
+        if(file.indexOf('.') === -1) dirs.push(`${file}/index.html`)
+        else purefiles.push(file)
+    })
+    const indexes = dirs.concat(purefiles)
+    fs.writeFile(path.join(__dirname, 'public/data/indexes.json'), JSON.stringify(indexes, null, '    '), (err) => {
+        if(err) throw err
+        console.log('filed created')
+    })
+})
